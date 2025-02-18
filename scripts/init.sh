46704df4b0564c676d3c093cc50639198513fdc5
daniel.andrade1991@gmail.com <daniel.andrade1991@gmail.com>
2025-02-18 01:27:01 -0300

    update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,8 +7,9 @@ source .env
 echo "🔄 Deteniendo servicios previos..."
 docker-compose down -v && sudo rm -rf .data && sudo rm -rf dist
 
-echo "Consultando las reservas para la inicialización de los pools..."
+echo "Inicializando datos"
 yarn pool-rsv
+yarn pairs-rsv
 
 echo "INICIANDO SOROSWAP SUBQUERY"
 yarn dev
