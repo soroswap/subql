3e24262c9280e6613a6ec6d41f09844ad03159a6
coderipper <hello@joaquinsoza.dev>
2025-03-07 11:55:37 -0400

    simplified version

--- project.ts
+++ project.ts
@@ -4,28 +4,30 @@ import {
   StellarProject,
 } from "@subql/types-stellar";
 import { startBlock } from "./scripts/lastLedger";
-import * as dotenv from 'dotenv';
-import path from 'path';
+import * as dotenv from "dotenv";
+import path from "path";
+import { Networks } from "@stellar/stellar-sdk";
 
-
-const mode = process.env.NODE_ENV || 'production';
+const mode = process.env.NODE_ENV || "production";
 
 // Load the appropriate .env file
-const dotenvPath = path.resolve(__dirname, `.env${mode !== 'production' ? `.${mode}` : ''}`);
+const dotenvPath = path.resolve(
+  __dirname,
+  `.env${mode !== "production" ? `.${mode}` : ""}`
+);
 dotenv.config({ path: dotenvPath });
-
 
 /* This is your project configuration */
 const project: StellarProject = {
   specVersion: "1.0.0",
-  name: "soroban-testnet-starter",
+  name: "soroswap-indexer",
   version: "0.0.1",
   runner: {
     node: {
       name: "@subql/node-stellar",
-      options: {
-        unsafe: true
-      },
+      // options: {
+      //   unsafe: true
+      // },
       version: "*",
     },
     query: {
@@ -33,9 +35,8 @@ const project: StellarProject = {
       version: "*",
     },
   },
-  description:
-    "This project can be use as a starting point for developing your new Stellar SubQuery project (testnet)",
-  repository: "https://github.com/subquery/stellar-subql-starter",
+  description: "Soroswap Indexer",
+  repository: "https://github.com/soroswap/subql",
   schema: {
     file: "./schema.graphql",
   },
@@ -44,7 +45,7 @@ const project: StellarProject = {
       'Test SDF Network ; September 2015' for testnet
       'Public Global Stellar Network ; September 2015' for mainnet
       'Test SDF Future Network ; October 2022' for Future Network */
-    chainId: process.env.CHAIN_ID!,
+    chainId: Networks.PUBLIC,
     /**
      * These endpoint(s) should be public non-pruned archive node
      * We recommend providing more than one endpoint for improved reliability, performance, and uptime
@@ -53,19 +54,22 @@ const project: StellarProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: process.env.HORIZON_ENDPOINT!?.split(',') as string[] | string,
+    endpoint: [
+      "https://newest-autumn-energy.stellar-mainnet.quiknode.pro/b9c096bbb70d53afa791ad08425ddb2f65fa2559",
+    ],
     /* This is a specific Soroban endpoint
       It is only required when you are using a soroban/EventHandler */
-    sorobanEndpoint: process.env.SOROBAN_ENDPOINT!,
+    sorobanEndpoint:
+      "https://newest-autumn-energy.stellar-mainnet.quiknode.pro/b9c096bbb70d53afa791ad08425ddb2f65fa2559",
   },
   dataSources: [
     {
       kind: StellarDatasourceKind.Runtime,
       /* Set this as a logical start block, it might be block 1 (genesis) or when your contract was deployed */
-      startBlock: parseInt(process.env.STARBLOCK!) || startBlock,
+      startBlock: startBlock,
       mapping: {
         file: "./dist/index.js",
-        handlers: [      
+        handlers: [
           {
             handler: "handleEventSync",
             kind: StellarHandlerKind.Event,
@@ -73,7 +77,7 @@ const project: StellarProject = {
               //contractId: "CDJDRGUCHANJDXALZVJ5IZVB76HX4MWCON5SHF4DE5HB64CBBR7W2ZCD",
               topics: [
                 "SoroswapPair",
-                "sync" // Topic para el evento sync
+                "sync", // Topic para el evento sync
               ],
             },
           },
@@ -81,11 +85,9 @@ const project: StellarProject = {
             handler: "handleEventNewPair",
             kind: StellarHandlerKind.Event,
             filter: {
-              contractId: process.env.FACTORY_CONTRACT_SOROSWAP as string,
-              topics: [
-                "SoroswapFactory",
-                "new_pair"
-              ],
+              contractId:
+                "CA4HEQTL2WPEUYKYKCDOHCDNIV4QHNJ7EL4J4NQ6VADP7SYHVRYZ7AW2",
+              topics: ["SoroswapFactory", "new_pair"],
             },
           },
         ],
