07441bdbe5467bb4fbf1a2725d5c0e9c310c28c1
daniel.andrade1991@gmail.com <daniel.andrade1991@gmail.com>
2025-05-05 12:33:39 -0400

    clean soroswapContracts

--- src/constants/soroswapContracts.ts
+++ src/constants/soroswapContracts.ts
@@ -3,17 +3,14 @@ import { NETWORK } from ".";
 const soroswapFactory = {
   mainnet: {
     address: "CA4HEQTL2WPEUYKYKCDOHCDNIV4QHNJ7EL4J4NQ6VADP7SYHVRYZ7AW2",
-    startBlock: 56931112,
   },
   testnet: {
     address: "CBVFAI4TEJCHIICFUYN2C5VYW5TD3CKPIZ4S5P5LVVUWMF5MRLJH77NH",
-    startBlock: 84371,
   },
 };
 
 export function getSoroswapFactory(network: NETWORK): {
   address: string;
-  startBlock: number;
 } {
   return soroswapFactory[network];
 }
