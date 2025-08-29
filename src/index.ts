1b502e86ea1e31b8e690c18e106b0a53e7ba8a60
esteblock <estebaniglesiasmanriquez@gmail.com>
2025-08-29 11:26:50 -0300

    add text encoder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+// Add `text-encoding` as a dependency and include this code in index.ts before any other imports
+import { TextDecoder, TextEncoder } from 'text-encoding';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";
+
+
